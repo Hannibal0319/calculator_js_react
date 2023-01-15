27aab201b1ecd21043a3a03248931bd58389269a
Hannibal0319 <13dzsoldosp@gmail.com>
2023-01-15 14:36:49 +0100

    Updates

---
+++ precache-manifest.98eab98a3fa6376cfbc5c39b79332c12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6404fb06483ddd2bda9",
+    "url": "/calculator_js_react/static/css/main.d44acd3c.chunk.css"
+  },
+  {
+    "revision": "e6404fb06483ddd2bda9",
+    "url": "/calculator_js_react/static/js/main.e6404fb0.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/calculator_js_react/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "2c90e21084ad62ed65ab",
+    "url": "/calculator_js_react/static/js/2.2c90e210.chunk.js"
+  },
+  {
+    "revision": "ae60cb88f142f605463e",
+    "url": "/calculator_js_react/static/js/runtime~main.ae60cb88.js"
+  },
+  {
+    "revision": "308df683a03dbb9a47387c59e80d8d04",
+    "url": "/calculator_js_react/index.html"
+  }
+];
